c58dc67e73075e826b62c1441f0c5956d3c03f80
ghyeon1946 <rudals1946@naver.com>
2021-01-14 23:15:57 +0900

    11279

---
+++ 11279.py
@@ -0,0 +1,25 @@
+import heapq
+import sys
+
+def Max_Heap(heap_items):
+    global n
+    
+    Max_heap = []
+    for i in range(n):
+        if heap_items[i] != 0:
+            heapq.heappush(Max_heap, (-heap_items[i], heap_items[i]))
+
+        else:
+            if not Max_heap:
+                print(0)
+            
+            else:
+                print(heapq.heappop(Max_heap)[1])
+
+n = int(input())
+
+heap_items = []
+for i in range(n):
+    heap_items.append(int(sys.stdin.readline()))
+
+Max_Heap(heap_items)
