aba6af61a9c3b9fa869aeab9a76f606d5d098e56
ghyeon1946 <rudals1946@naver.com>
2021-01-14 16:58:17 +0900

    2630

--- 2630.py
+++ 2630.py
@@ -24,7 +24,7 @@ def Paper(x, y, N):
             white += 1
         elif a == 0:
             black += 1
-
+ 
         if b == 1:
             white += 1
         elif b == 0:
@@ -40,23 +40,13 @@ def Paper(x, y, N):
         elif d == 0:
             black += 1
     return 2
-
-def Answer(x, y, N):
-    global white, black
     
-    if not Paper(x, y, N) == 2:
-        if Paper(x, y, N) == 1:
-            white += 1
-
-        elif Paper(x, y, N) == 0:
-            black += 1
-
 n = int(input())
 
 for i in range(n):
     paper.append(list(map(int, input().split())))
 
-Answer(0, 0, n)
+Paper(0, 0, n)
 
 print(black)
 print(white)
